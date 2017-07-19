ecc4d438a367cf1835ae8ba1233c0a4c71f46855
Ahmad Eldefrawy <ahmad.eldefrawy@gmail.com>
2017-07-19 16:59:04 +0200

    Added LeetCode Directory

---
+++ LeetCode/TwoSum.java
@@ -0,0 +1,31 @@
+package leetCode.easy;
+
+import java.util.HashMap;
+
+/**
+ * Created by Ahmad on 7/19/2017.
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] input = {11, 7, 15, 2};
+        int[] ouput = twoSum(input, 9);
+
+        System.out.println(ouput[0]);
+        System.out.println(ouput[1]);
+    }
+
+    // SOLUTION
+    public static int[] twoSum(int[] nums, int target) {
+        int[] output = new int[2];
+        HashMap<Integer, Integer> arrayHS = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (arrayHS.containsKey(target - nums[i])) {
+                output[0] = arrayHS.get(target - nums[i]);
+                output[1] = i;
+            }
+            arrayHS.put(nums[i], i);
+        }
+        return output;
+    }
+}
